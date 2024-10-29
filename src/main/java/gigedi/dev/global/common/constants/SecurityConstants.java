352bb9e325addf42f11f7521cbeee7d156281cb6
iiqcov <iiqcov@gmail.com>
2024-10-29 17:06:13 +0900

    feat : 로그인 관련 상수 추가

---
+++ src/main/java/gigedi/dev/global/common/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package gigedi.dev.global.common.constants;
+
+public final class SecurityConstants {
+    public static final String GET_ID_TOKEN_URL = "https://oauth2.googleapis.com/token";
+    public static final String GOOGLE_ISSUER = "https://accounts.google.com";
+    public static final String GOOGLE_JWK_SET_URL = "https://www.googleapis.com/oauth2/v3/certs";
+
+    public static final String TOKEN_ROLE_NAME = "role";
+}
