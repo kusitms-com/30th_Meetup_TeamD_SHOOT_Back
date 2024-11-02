b2f68c5c3342aef76e2ef52224dbe28d4a840fd6
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-03 03:41:13 +0900

    feat : 블록 생성 API 구현

--- src/main/java/gigedi/dev/domain/auth/dao/FigmaRepository.java
+++ src/main/java/gigedi/dev/domain/auth/dao/FigmaRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import gigedi.dev.domain.auth.domain.Figma;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FigmaRepository extends JpaRepository<Figma, Long> {
     Optional<Figma> findByMemberId(Long memberId);
     Optional<Figma> findByFigmaUserId(String figmaUserId);
