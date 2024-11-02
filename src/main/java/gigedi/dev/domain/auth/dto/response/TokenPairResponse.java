95084b1cbb5421ecb1e44eaa3ac29e62e75bd5b4
iiqcov <iiqcov@gmail.com>
2024-11-03 00:37:48 +0900

    fix : 토큰 반환 순서 변경

--- src/main/java/gigedi/dev/domain/auth/dto/response/TokenPairResponse.java
+++ src/main/java/gigedi/dev/domain/auth/dto/response/TokenPairResponse.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class TokenPairResponse {
-    private final String refreshToken;
     private final String accessToken;
+    private final String refreshToken;
 }
