b574d5a54cb793118934bfb8ca957f353b518f3d
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-11-07 23:03:24 +0900

    [Feat] 피그마 헤더 정보 인터셉터 및 유틸 추가 (#32)
    * feat : 피그마 정보 인터셉터 추가
    
    * refactor : 인터셉터 변수 상수화
    
    * refactor : 중복코드 함수화

---
+++ src/main/java/gigedi/dev/domain/file/dao/AuthorityRepository.java
@@ -0,0 +1,15 @@
+package gigedi.dev.domain.file.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gigedi.dev.domain.auth.domain.Figma;
+import gigedi.dev.domain.file.domain.Authority;
+import gigedi.dev.domain.file.domain.File;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+    Optional<Authority> findByFigmaAndFile(Figma figma, File file);
+}
