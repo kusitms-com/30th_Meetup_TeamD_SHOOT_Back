a9d7484ce7a85103ef3837ab6e0b08f2046792c5
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-07 18:00:46 +0900

    feat : figma CORS 설정

--- src/main/java/gigedi/dev/global/config/WebConfig.java
+++ src/main/java/gigedi/dev/global/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000", "http://localhost:3000")
+                .allowedOriginPatterns(
+                        "https://localhost:3000",
+                        "http://localhost:3000",
+                        "https://www.figma.com/**")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
