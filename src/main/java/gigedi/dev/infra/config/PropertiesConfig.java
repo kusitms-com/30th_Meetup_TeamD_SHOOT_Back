020956fa37e9cfa356517d0b07334d897476c317
iiqcov <iiqcov@gmail.com>
2024-11-14 01:41:48 +0900

    chore : 디스코드 환경변수 추가

--- src/main/java/gigedi/dev/infra/config/PropertiesConfig.java
+++ src/main/java/gigedi/dev/infra/config/PropertiesConfig.java
@@ -4,8 +4,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 import gigedi.dev.infra.config.jwt.JwtProperties;
+import gigedi.dev.infra.config.oauth.DiscordProperties;
 import gigedi.dev.infra.config.oauth.GoogleProperties;
 
 @Configuration
-@EnableConfigurationProperties({GoogleProperties.class, JwtProperties.class})
+@EnableConfigurationProperties({
+    GoogleProperties.class,
+    JwtProperties.class,
+    DiscordProperties.class
+})
 public class PropertiesConfig {}
