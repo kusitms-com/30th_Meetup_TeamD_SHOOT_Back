2fe8c51d2591ee4b7e06950146b158a390b6d380
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-16 18:46:10 +0900

    feat : TimeUtil로 Duration 계산 메서드 분리

--- src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
+++ src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
@@ -1,7 +1,5 @@
 package gigedi.dev.domain.shoot.application;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +11,7 @@ import gigedi.dev.domain.shoot.domain.Shoot;
 import gigedi.dev.domain.shoot.domain.ShootStatus;
 import gigedi.dev.domain.shoot.domain.Status;
 import gigedi.dev.domain.shoot.dto.response.GetShootResponse;
+import gigedi.dev.global.util.TimeUtil;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -20,11 +19,6 @@ import lombok.RequiredArgsConstructor;
 public class ShootService {
     private final ShootRepository shootRepository;
     private final ShootStatusRepository shootStatusRepository;
-    private static final String MINUTES_AGO = " minutes ago";
-    private static final String HOURS_AGO = " hours ago";
-    private static final String DAYS_AGO = " days ago";
-    private static final long MINUTES_IN_AN_HOUR = 60;
-    private static final long HOURS_IN_A_DAY = 24;
 
     public List<GetShootResponse> getShoot(Long blockId) {
         List<Shoot> shoots = shootRepository.findAllByBlock_BlockId(blockId);
@@ -32,7 +26,7 @@ public class ShootService {
         return shoots.stream()
                 .map(
                         shoot -> {
-                            String timeAgo = calculateTimeAgo(shoot.getCreatedAt());
+                            String timeAgo = TimeUtil.calculateTimeAgo(shoot.getCreatedAt());
 
                             List<GetShootResponse.User> yet = getUsersByStatus(shoot, "YET");
                             List<GetShootResponse.User> doing = getUsersByStatus(shoot, "DOING");
@@ -52,36 +46,18 @@ public class ShootService {
                 .collect(Collectors.toList());
     }
 
-    private String calculateTimeAgo(LocalDateTime createdAt) {
-        Duration duration = Duration.between(createdAt, LocalDateTime.now());
-        long minutes = duration.toMinutes();
-        long hours = duration.toHours();
-        long days = duration.toDays();
-
-        if (minutes < MINUTES_IN_AN_HOUR) {
-            return minutes + MINUTES_AGO;
-        } else if (hours < HOURS_IN_A_DAY) {
-            return hours + HOURS_AGO;
-        } else {
-            return days + DAYS_AGO;
-        }
-    }
-
     private List<GetShootResponse.User> getUsersByStatus(Shoot shoot, String status) {
-        // ShootStatus를 shootId와 상태로 조회
         List<ShootStatus> statuses =
                 shootStatusRepository.findByShoot_ShootIdAndStatus(
                         shoot.getShootId(), Status.valueOf(status));
 
-        // 조회된 데이터를 User DTO로 매핑
         return statuses.stream()
                 .map(
                         s ->
                                 new GetShootResponse.User(
-                                        s.getFigma().getFigmaId(), // Figma ID
-                                        s.getFigma().getFigmaName(), // Figma 이름
-                                        s.getFigma().getFigmaProfile() // Figma 프로필
-                                        ))
+                                        s.getFigma().getFigmaId(),
+                                        s.getFigma().getFigmaName(),
+                                        s.getFigma().getFigmaProfile()))
                 .collect(Collectors.toList());
     }
 }
