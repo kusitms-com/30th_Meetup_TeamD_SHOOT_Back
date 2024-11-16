e93255c05289a6e0c98a92b07602d5fe0035e7c7
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-16 19:00:05 +0900

    feat : convert 로직 dto로 이동

--- src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
+++ src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
@@ -1,17 +1,14 @@
 package gigedi.dev.domain.shoot.application;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import gigedi.dev.domain.shoot.dao.ShootRepository;
 import gigedi.dev.domain.shoot.dao.ShootStatusRepository;
 import gigedi.dev.domain.shoot.domain.Shoot;
-import gigedi.dev.domain.shoot.domain.ShootStatus;
 import gigedi.dev.domain.shoot.domain.Status;
 import gigedi.dev.domain.shoot.dto.response.GetShootResponse;
-import gigedi.dev.global.util.TimeUtil;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -25,39 +22,15 @@ public class ShootService {
 
         return shoots.stream()
                 .map(
-                        shoot -> {
-                            String timeAgo = TimeUtil.calculateTimeAgo(shoot.getCreatedAt());
-
-                            List<GetShootResponse.User> yet = getUsersByStatus(shoot, "YET");
-                            List<GetShootResponse.User> doing = getUsersByStatus(shoot, "DOING");
-                            List<GetShootResponse.User> done = getUsersByStatus(shoot, "DONE");
-
-                            return new GetShootResponse(
-                                    shoot.getShootId(),
-                                    shoot.getFigma().getFigmaName(),
-                                    shoot.getFigma().getFigmaProfile(),
-                                    shoot.getContent(),
-                                    timeAgo,
-                                    yet,
-                                    doing,
-                                    done,
-                                    shoot.getCreatedAt().toString());
-                        })
-                .collect(Collectors.toList());
-    }
-
-    private List<GetShootResponse.User> getUsersByStatus(Shoot shoot, String status) {
-        List<ShootStatus> statuses =
-                shootStatusRepository.findByShoot_ShootIdAndStatus(
-                        shoot.getShootId(), Status.valueOf(status));
-
-        return statuses.stream()
-                .map(
-                        s ->
-                                new GetShootResponse.User(
-                                        s.getFigma().getFigmaId(),
-                                        s.getFigma().getFigmaName(),
-                                        s.getFigma().getFigmaProfile()))
-                .collect(Collectors.toList());
+                        shoot ->
+                                GetShootResponse.from(
+                                        shoot,
+                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
+                                                shoot.getShootId(), Status.YET),
+                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
+                                                shoot.getShootId(), Status.DOING),
+                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
+                                                shoot.getShootId(), Status.DONE)))
+                .toList();
     }
 }
--- src/main/java/gigedi/dev/domain/shoot/dto/response/GetShootResponse.java
+++ src/main/java/gigedi/dev/domain/shoot/dto/response/GetShootResponse.java
@@ -1,6 +1,11 @@
 package gigedi.dev.domain.shoot.dto.response;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import gigedi.dev.domain.shoot.domain.Shoot;
+import gigedi.dev.domain.shoot.domain.ShootStatus;
+import gigedi.dev.global.util.TimeUtil;
 
 public record GetShootResponse(
         Long shootId,
@@ -13,4 +18,32 @@ public record GetShootResponse(
         List<User> done,
         String createdAt) {
     public record User(Long userId, String username, String profileImg) {}
+
+    public static GetShootResponse from(
+            Shoot shoot,
+            List<ShootStatus> yetStatuses,
+            List<ShootStatus> doingStatuses,
+            List<ShootStatus> doneStatuses) {
+        return new GetShootResponse(
+                shoot.getShootId(),
+                shoot.getFigma().getFigmaName(),
+                shoot.getFigma().getFigmaProfile(),
+                shoot.getContent(),
+                TimeUtil.calculateTimeAgo(shoot.getCreatedAt()),
+                convertToUsers(yetStatuses),
+                convertToUsers(doingStatuses),
+                convertToUsers(doneStatuses),
+                shoot.getCreatedAt().toString());
+    }
+
+    private static List<User> convertToUsers(List<ShootStatus> statuses) {
+        return statuses.stream()
+                .map(
+                        status ->
+                                new User(
+                                        status.getFigma().getFigmaId(),
+                                        status.getFigma().getFigmaName(),
+                                        status.getFigma().getFigmaProfile()))
+                .collect(Collectors.toList());
+    }
 }
