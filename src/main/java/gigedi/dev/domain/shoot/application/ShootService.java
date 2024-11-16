efd2a11e4c822bbde5e37d39dcdc649b5197dcc4
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-16 19:03:10 +0900

    feat : dto, service 정리

--- src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
+++ src/main/java/gigedi/dev/domain/shoot/application/ShootService.java
@@ -1,9 +1,11 @@
 package gigedi.dev.domain.shoot.application;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import gigedi.dev.domain.auth.domain.Figma;
 import gigedi.dev.domain.shoot.dao.ShootRepository;
 import gigedi.dev.domain.shoot.dao.ShootStatusRepository;
 import gigedi.dev.domain.shoot.domain.Shoot;
@@ -22,15 +24,28 @@ public class ShootService {
 
         return shoots.stream()
                 .map(
-                        shoot ->
-                                GetShootResponse.from(
-                                        shoot,
-                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
-                                                shoot.getShootId(), Status.YET),
-                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
-                                                shoot.getShootId(), Status.DOING),
-                                        shootStatusRepository.findByShoot_ShootIdAndStatus(
-                                                shoot.getShootId(), Status.DONE)))
+                        shoot -> {
+                            return GetShootResponse.from(
+                                    shoot,
+                                    getUsersByStatus(shoot, Status.YET),
+                                    getUsersByStatus(shoot, Status.DOING),
+                                    getUsersByStatus(shoot, Status.DONE));
+                        })
                 .toList();
+    }
+
+    private List<GetShootResponse.User> getUsersByStatus(Shoot shoot, Status status) {
+        return shootStatusRepository
+                .findByShoot_ShootIdAndStatus(shoot.getShootId(), status)
+                .stream()
+                .map(
+                        shootStatus -> {
+                            Figma figma = shootStatus.getFigma();
+                            return new GetShootResponse.User(
+                                    figma.getFigmaId(),
+                                    figma.getFigmaName(),
+                                    figma.getFigmaProfile());
+                        })
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/gigedi/dev/domain/shoot/dto/response/GetShootResponse.java
+++ src/main/java/gigedi/dev/domain/shoot/dto/response/GetShootResponse.java
@@ -1,10 +1,8 @@
 package gigedi.dev.domain.shoot.dto.response;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import gigedi.dev.domain.shoot.domain.Shoot;
-import gigedi.dev.domain.shoot.domain.ShootStatus;
 import gigedi.dev.global.util.TimeUtil;
 
 public record GetShootResponse(
@@ -20,30 +18,16 @@ public record GetShootResponse(
     public record User(Long userId, String username, String profileImg) {}
 
     public static GetShootResponse from(
-            Shoot shoot,
-            List<ShootStatus> yetStatuses,
-            List<ShootStatus> doingStatuses,
-            List<ShootStatus> doneStatuses) {
+            Shoot shoot, List<User> yetUsers, List<User> doingUsers, List<User> doneUsers) {
         return new GetShootResponse(
                 shoot.getShootId(),
                 shoot.getFigma().getFigmaName(),
                 shoot.getFigma().getFigmaProfile(),
                 shoot.getContent(),
                 TimeUtil.calculateTimeAgo(shoot.getCreatedAt()),
-                convertToUsers(yetStatuses),
-                convertToUsers(doingStatuses),
-                convertToUsers(doneStatuses),
+                yetUsers,
+                doingUsers,
+                doneUsers,
                 shoot.getCreatedAt().toString());
-    }
-
-    private static List<User> convertToUsers(List<ShootStatus> statuses) {
-        return statuses.stream()
-                .map(
-                        status ->
-                                new User(
-                                        status.getFigma().getFigmaId(),
-                                        status.getFigma().getFigmaName(),
-                                        status.getFigma().getFigmaProfile()))
-                .collect(Collectors.toList());
     }
 }
