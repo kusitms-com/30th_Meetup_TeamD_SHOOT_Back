c6ad9c1caf31b0234f222051aade22f774ba4ddf
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-23 22:13:38 +0900

    feat : redis 추가

---
+++ src/main/java/gigedi/dev/infra/config/redis/RedisProperties.java
@@ -0,0 +1,6 @@
+package gigedi.dev.infra.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public record RedisProperties(String host, int port, String password) {}
