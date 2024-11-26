ea1af5cc315b717bf8343eadbaf05aa46ac88ef9
iiqcov <iiqcov@gmail.com>
2024-11-27 01:01:43 +0900

    fix : jsonignore 추가

--- src/main/java/gigedi/dev/domain/auth/dto/response/FigmaAccountResponse.java
+++ src/main/java/gigedi/dev/domain/auth/dto/response/FigmaAccountResponse.java
@@ -1,3 +1,6 @@
 package gigedi.dev.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FigmaAccountResponse(String figmaId, String email) {}
